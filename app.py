127358c0b96d9ffd88094675e1a034b3181b50bd
Saadc <saadchraibi85@gmail.com>
2022-07-24 12:10:45 +0100

    I update myc hanges

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
 
-# Intitialise the app
 app = Flask(__name__)
 
-# Define what the app does
 @app.get("/greet")
 def index():
-    """
-    TODO:
-    1. Capture first name & last name
-    2. If either is not provided: respond with an error
-    3. If first name is not provided and second name is provided: respond with "Hello Mr <second-name>!"
-    4. If first name is provided byt second name is not provided: respond with "Hello, <first-name>!"
-    5. If both names are provided: respond with a question, "Is your name <fist-name> <second-name>
-    """
-    return jsonify("TODO")
+    fname = request.args.get("fname")
+    lname = request.args.get("lname")
+    if not fname and not lname:
+        return jsonify({"status":"error"})
+    elif not fname and lname:
+        response = {"data": f"Hello, mr {lname}"}
+    
+    elif not lname and fname:
+        response = {"data": f"Heyyy, {fname}"}
+    elif lname and fname:
+        response = {"data": f"Is that you, {fname} {lname}"}
+
+    
+    return jsonify(response)
